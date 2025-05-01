5457ff9c2e086f1a078fe16a968d138d7aafeff6
Jayprakash Sahu <118419073+Jpfpp12@users.noreply.github.com>
2025-05-01 12:09:28 +0530

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,11 @@
 import React from 'react';
 
 function Home() {
-  return <h1>Welcome to my website!</h1>;
+  return (
+    <div>
+      <h1>Welcome to my website!</h1>
+    </div>
+  );
 }
 
 export default Home;
