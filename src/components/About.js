cfd67c50e871aa80ec719f8a76518ea084d11f9e
Jayprakash Sahu <118419073+Jpfpp12@users.noreply.github.com>
2025-05-01 12:09:00 +0530

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -2,8 +2,11 @@ import React from 'react';
 
 function About() {
   return (
-    <h1>About</h1>;
-    <p>This is a sample React Router program.</p>
-)}
+    <div>
+      <h1>About</h1>
+      <p>This is a sample React Router program.</p>
+    </div>
+  );
+}
 
 export default About;
