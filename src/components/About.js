db3b66ca5f6cad09ce0f9b1fe07df6531cf22b75
Jayprakash Sahu <118419073+Jpfpp12@users.noreply.github.com>
2025-05-01 11:52:33 +0530

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -1,3 +1,7 @@
-export default function About() {
-  return <h1>About Us</h1>;
+import React from 'react';
+
+function About() {
+  return <h1>This is a sample React Router program.</h1>;
 }
+
+export default About;
